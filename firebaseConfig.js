ae6747838b5f6207ea7ddeb909f072ee8841c1b3
Daddy4AI <alanqwyuan@gmail.com>
2024-12-01 23:05:04 +0800

    initial

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAE5OL3jOqHrXz4VRlN-Im3sDyiMJq2PG0",
+  authDomain: "entry001.firebaseapp.com",
+  databaseURL: "https://entry001-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "entry001",
+  storageBucket: "entry001.firebasestorage.app",
+  messagingSenderId: "39651467700",
+  appId: "1:39651467700:web:05392de9285dfcf72d12e1"
+};
+
+
+export default firebaseConfig;
